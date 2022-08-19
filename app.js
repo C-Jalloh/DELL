ec25d81cd6401a5d66d575dc6208daaa217a4251
SarahKapalka <sarahnj73bc@gmail.com>
2022-08-19 18:02:37 +0000

    case insensitive and ?! acceptance

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 function talk(){
     var know = {
-        "Who are you" : "Hello, C Jalloh's Chatbot here ",
-        "How are you" : "Good :)",
-        "What can I do for you":"Please give me a like on my Socials @CJalloh25 and check out my youtube channel @CTech",
-        "Your followers" : "I have my family of 5000 members, I don't have followers, I have a supportive Family ",
-        "Ok" : "Thank you so much",
-        "Bye" : "Okay! We'll meet soon.."
+        "who are you" : "Hello, C Jalloh's Chatbot here ",
+        "how are you" : "Good :)",
+        "what can I do for you":"Please give me a like on my Socials @CJalloh25 and check out my youtube channel @CTech",
+        "your followers" : "I have my family of 5000 members, I don't have followers, I have a supportive Family ",
+        "ok" : "Thank you so much",
+        "bye" : "Okay! We'll meet soon.."
     };
-    var user = document.getElementById("userBox").value;
+    var user = document.getElementById("userBox").value.toLowerCase();
+    const regex= /[!\?]+/g;
+    if(regex.test(user)) user = user.replace(regex,"");
     document.getElementById('chatLog').innerHTML = user + "<br>";
     if (user in know) {
         document.getElementById('chatLog').innerHTML = know[user] + "<br>";
